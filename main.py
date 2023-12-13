8dedc8e26f8727e536d41df5b309c0484976b9f6
Skstud5 <skripchenko@rb.asu.ru>
2023-12-13 14:52:27 +0700

    update (main) Add decorators

--- main.py
+++ main.py
@@ -5,14 +5,11 @@ app = FastAPI()
 
 @app.get("/")
 async def root_page():
-    return {
-        "message": "World___",
-        "keyroot": "Приветствую!"
-    }
+    return "Скрипченко Сергей"
 @app.get("/tools")
 async def tools_page():
-    return "Страница Tools"
+    return "Я являюсь опытным программистом на Python с фокусом на веб-разработку и автоматизацию. Мой опыт включает в себя создание эффективных и масштабируемых веб-приложений, а также решение сложных задач с использованием современных технологий."
 
 @app.get("/users")
 async def users_page():
-    return "Страница Users"
+    return "Телефон: 8 800 555 35 35"
