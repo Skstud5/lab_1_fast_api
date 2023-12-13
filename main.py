8a5515a7f54ced0c70f09e3c044a88902f0a4d58
Skstud5 <skripchenko@rb.asu.ru>
2023-12-13 14:44:55 +0700

    update (main) Add decorators

--- main.py
+++ main.py
@@ -4,5 +4,15 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root_page():
+    return {
+        "message": "World___",
+        "keyroot": "Приветствую!"
+    }
+@app.get("/tools")
+async def tools_page():
+    return "Страница Tools"
+
+@app.get("/users")
+async def users_page():
+    return "Страница Users"
